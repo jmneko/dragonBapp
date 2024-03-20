6459dfb881779e5fc0150147437339d5876f8c30
jmneko <jmunhiztarrio@gmail.com>
2024-03-21 00:25:47 +0100

    creacion de vistapersonaje

---
+++ src/app/pages/vista-personaje/vista-personaje.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Personaje } from 'src/app/interfaces/personaje.interfaces';
+import { DragonballService } from 'src/app/services/dragonball.service';
+
+@Component({
+  selector: 'app-vista-personaje',
+  templateUrl: './vista-personaje.component.html',
+  styleUrls: ['./vista-personaje.component.css']
+})
+export class VistaPersonajeComponent {
+
+  @Input() personajeActual: Personaje | any;
+  personajeSolo: Personaje | any;
+
+  activatedRoute = inject(ActivatedRoute);
+  personajesServices = inject(DragonballService);
+
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: any) => {
+      let id = String(params.idpersonaje);
+      this.personajeSolo = this.personajesServices.getPersonajeId(id).subscribe((response) => {
+        this.personajeSolo = response;
+        console.log(response)
+      })
+    })
+  }
+
+}
